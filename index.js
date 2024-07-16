a908040e316f171a8ee6529d8978f214f67f3346
angelopol <114349306+angelopol@users.noreply.github.com>
2024-07-15 21:33:22 -0400

    Calculadora de est

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-console.log("en una clase de 21 alumnos, van a distribuirse tres premios. Averiguar como van a distribuirse si: los premios son diferentes o si los premios son iguales.")
-
 function factorial(n){
     var total =1;
     for(i=1;i<=n;i++){
@@ -35,23 +33,49 @@ function permutacionRep(numero,rep){
         factorial(numero)/(acum)
     )
 }
-//Ejercicio 2 (que hicimosd en clase)
-console.log("A) "+combinacion(21,3))
 
-console.log("B) "+permutacion(21,3))
+var buttonfact = document.getElementById('buttonfact');
+var buttomcom = document.getElementById('buttomcom');
+var buttonper = document.getElementById('buttonper');
+var buttonperrep = document.getElementById('buttonperrep');
+var fact = document.getElementById('fact');
+var com = document.getElementById('com');
+var per = document.getElementById('per');
+var perrep = document.getElementById('perrep');
 
-var a1 = document.getElementById('A1');
-var b1 = document.getElementById('B1');
-var a1b = document.getElementById('A1B');
-var b1b = document.getElementById('B1B');
-
-a1.addEventListener('click', () => {
-    a1b.innerHTML += '<p class="fw-bold pt-4 fs-4">' + combinacion(21,3) + " combinaciones</p>"
+buttonfact.addEventListener('click', () => {
+    fact.innerHTML += '<p class="fw-bold pt-4 fs-4">' + factorial(parseFloat(document.getElementById('numfact').value)) + " factorial</p>"
 })
 
-b1.addEventListener('click', () => {
-    b1b.innerHTML += '<p class="fw-bold pt-4 fs-4">' + permutacion(21,3) + " permutaciones</p>"
+buttomcom.addEventListener('click', () => {
+    com.innerHTML += '<p class="fw-bold pt-4 fs-4">' + combinacion(parseFloat(document.getElementById('totalcom').value),parseFloat(document.getElementById('elegidoscom').value)) + " combinaciones</p>"
 })
+
+buttonper.addEventListener('click', () => {
+    per.innerHTML += '<p class="fw-bold pt-4 fs-4">' + permutacion(parseFloat(document.getElementById('totalper').value),parseFloat(document.getElementById('elegidosper').value)) + " permutaciones</p>"
+})
+
+buttonperrep.addEventListener('click', () => {
+    perrep.innerHTML += '<p class="fw-bold pt-4 fs-4">' + permutacionRep(parseFloat(document.getElementById('totalperrep').value),parseFloat(document.getElementById('elegidosperrep').value)) + " permutaciones con repetición</p>"
+})
+
+function AddEjercicio(){
+    const ejercicios = document.getElementById('ejercicios');
+    ejercicios.innerHTML += '<div class="card mt-4"><div class="card-body"><h5 class="card-title">Ejercicio 1</h5><p class="card-text">¿De cuántas maneras pueden sentarse 10 personas en un banco si hay 4 sitios disponibles?</p><p class="card-text">Respuesta: '+permutacion(10,4)+' maneras</p></div></div>';
+    ejercicios.innerHTML += '<div class="card mt-4"><div class="card-body"><h5 class="card-title">Ejercicio 3</h5><p class="card-text">Las diagonales de un polígono se obtienen uniendo pares de vértices no adyacentes.<br> 1) Obtener el número de diagonales del cuadrado, hexágono y decagono.</p><p class="card-text">Cuadrado: <br>-Primero se calculan las uniones posible: '+combinacion(4,2)+'<br>Diagonales: '+(combinacion(4,2)-4)+' diagonales</p><p class="card-text">Hexagono: <br>-Primero se calculan las uniones posible: '+combinacion(6,2)+'<br>Diagonales: '+(combinacion(6,2)-6)+' diagonales</p><p class="card-text">Decagono: <br>-Primero se calculan las uniones posible: '+combinacion(10,2)+'<br>Diagonales: '+(combinacion(10,2)-10)+' diagonales</p></div></div>';
+    ejercicios.innerHTML += '<div class="card mt-4"><div class="card-body"><h5 class="card-title">Ejercicio 4</h5><p class="card-text">Hay que colocar a 8 hombres y 7 mujeres en una fila de modo que las mujeres ocupen los lugares pares. De cuantas maneras pueden hacerse?</p><p class="card-text">Formas de colocarse los hombres: '+factorial(8)+'<br>Fomas de colocarse las mujeres: '+factorial(7)+'<br>Respuesta: '+(factorial(8)*factorial(7))+'</p></div></div>';
+    ejercicios.innerHTML += '<div class="card mt-4"><div class="card-body"><h5 class="card-title">Ejercicio 5</h5><p class="card-text">Cuantos números de 4 dígitos se pueden formar con las cifras 1,2,…9<br>1) Permitiendo repeticiones <br>2) Sin repeticiones <br>3) Si el ultimo digito ha de ser 1 y no se permiten repeticiones</p><p class="card-text">1) Respuesta: '+(9**4) + '<br>2) Respuesta: '+permutacion(9,4)+'<br> Respuesta: '+combinacion(8,3)+'</p></div></div>';
+    ejercicios.innerHTML += '<div class="card mt-4"><div class="card-body"><h5 class="card-title">Ejercicio 6</h5><p class="card-text">En un grupo de 15 amigos ¿Cuántas distribuciones de sus fechas de cumpleaños pueden darse?</p><p class="card-text">Respuesta: '+(365**15)+'</p></div></div>';
+    ejercicios.innerHTML += '<div class="card mt-4"><div class="card-body"><h5 class="card-title">Ejercicio 7</h5><p class="card-text">Cuantas letras de 5 signos con 3 rayas y 2 puntos podrían tener el alfabeto Morse?</p><p class="card-text">Respuesta: '+permutacionRep(5,[3,2])+' letras</p></div></div>';
+    ejercicios.innerHTML += '<div class="card mt-4"><div class="card-body"><h5 class="card-title">Ejercicio 8</h5><p class="card-text">Cuando se arrojan simultáneamente 4 monedas:<br>a) ¿Cuáles son los resultados posibles que se pueden obtener?<br>b) ¿Cuántos casos hay en que salgan 2 caras y 2 cruces?</p><p class="card-text">a) Respuesta (no importa el orden): '+combinacion(4+2-1,4)+'<br>b) Respuesta: '+combinacion(4,2)+'</p></div></div>';
+    ejercicios.innerHTML += '<div class="card mt-4"><div class="card-body"><h5 class="card-title">Ejercicio 9</h5><p class="card-text">5 libros de matemáticas, 7 de control de proyecto y 3 de interfaces han de ser colocados en una biblioteca. Cuántas colocaciones distintas admiten si:<br>a) Los libros de cada materia han de estar juntos.<br>b) Sólo los de matemáticas tienen que estar juntos.</p><p class="card-text">Ordenaciones posibles de las materias: '+factorial(3)+'<br>Permutaciones de los libros de matematica: '+permutacion(5,5)+'<br>Permutaciones de los libros de control de proyectos: '+permutacion(7,7)+'<br>Permutaciones de los libros de interfaz: '+permutacion(3,3)+'</p><p class="card-text">a) Respuesta (libros diferentes): '+(factorial(3)*permutacion(5,5)*permutacion(7,7)*permutacion(3,3))+'</p><p class="card-text">b) Como solo los de matematica han de estar solos tenemos 11 unidades (1 matematica,7 control de proyecto,3 interfaces) entonces tenemos: '+factorial(11)+' maneras de ordenarlas.<br>Ordenaciones posibles de los libros de matematica: '+factorial(5)+'<br>Respuesta: '+(factorial(11)*factorial(5))+' colocaciones</p></div></div>';
+    ejercicios.innerHTML += '<div class="card mt-4"><div class="card-body"><h5 class="card-title">Ejercicio 11</h5><p class="card-text">Un alumno tiene que elegir 7 de las 10 preguntas de un examen. ¿De cuantas maneras puede elegirlas? ¿Y si las 4 primeras son obligatorias?</p><p class="card-text">a) '+combinacion(10,7)+' maneras</p><p class="card-text">b) '+combinacion(6,3)+' maneras si las primeras 4 son obligatorias</p></div></div>';
+    ejercicios.innerHTML += '<div class="card mt-4"><div class="card-body"><h5 class="card-title">Ejercicio 12</h5><p class="card-text">Una línea de ferrocarril tiene 25 estaciones. ¿Cuántos billetes diferentes habrá que imprimir si cada billete lleva impresas las estaciones de origen y destino?</p><p class="card-text">Respuesta: '+permutacion(25,2)+' billetes diferentes</p></div></div>';
+    ejercicios.innerHTML += '<div class="card mt-4"><div class="card-body"><h5 class="card-title">Ejercicio 13</h5><p class="card-text">Tres atletas toman parte en una competición. ¿De cuántas maneras podrán llegar a la meta? (Pueden llegar juntos)</p><p class="card-text">Si llegan juntos: 1<br>Si llegan 2 juntos: 6<br>Si llegan separados: '+factorial(3)+'</p><p class="card-text">Respuesta: 13</p></div></div>';
+    ejercicios.innerHTML += '<div class="card mt-4"><div class="card-body"><h5 class="card-title">Ejercicio 14</h5><p class="card-text">En un hospital se utilizan cinco símbolos para clasificar las historias clínicas de sus pacientes, de manera que los dos primeros son letras y los tres últimos son dígitos. Suponiendo que hay 25 letras, ¿cuántas historias clínicas podrían hacerse si: <br>1. No hay restricciones sobre letras y números; <br>2. Las dos letras no pueden ser iguales.</p><p class="card-text">1) '+(25**2)+' posibilidades para las letras y '+(10**3)+' posibilidades para los digitos<br>Respuesta: '+(25**2*10**3)+'<br>2) '+permutacion(25,2)+' posibilidades para las letras y '+(10**3)+' posibilidades para los digitos<br>Respuesta: '+(permutacion(25,2)*10**3)+'</p></div></div>';
+}
+
+AddEjercicio();
 
 //Ejercicio 1
 console.log("1. ¿De cuántas maneras pueden sentarse 10 personas en un banco si hay 4 sitios disponibles?")
